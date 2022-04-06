03e5544a0845ec8fd085b7ae85ad2d8587a860f5
bigjohn98pl-PC <56130048+bigjohn98pl@users.noreply.github.com>
2022-04-06 18:24:13 +0200

    Dodanie testu sprawdzajacego metode showMenu w main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
 #include "header.hpp"
 #include "menu.hpp"
-#include "card.hpp"
 
 int main()
 {
     cout << "Witamy w naszej Pizzeri!" << endl;
     menu _Menu;
-    card _card;
+    _Menu.showMenu();
     return 0;
 }
